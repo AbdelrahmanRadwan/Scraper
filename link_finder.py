f97cae112c86886efffdfdce5c349548a06de649
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-04-20 21:28:22 +0200

    added the link finder class

---
+++ link_finder.py
@@ -0,0 +1,23 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+    def __int__(self, base_url, page_url):
+        super().__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
+    #overriding the handling start tag function
+    def handle_starttag(self, tag, attrs):
+        if tag =="a":
+            for attribute, value in attrs:
+                if attribute == "href":
+                    url = parse.urljoin(self.base_url, value)
+                    self.links.add(url)
+                    print(url)
+    #Returns all the links crawled
+    def page_links(self):
+        return self.links
+    #Error handler
+    def error(self, message):
+        pass
