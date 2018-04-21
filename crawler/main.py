429d7b25729e3665ef09bba4e1a46b375ee8eb94
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-04-21 18:05:43 +0200

    crawling youtube home page

--- crawler/main.py
+++ crawler/main.py
@@ -3,8 +3,8 @@ from queue import Queue
 from Spider.spider import spider
 from helper_functions.domain import *
 
-PROJECT_NAME = "projects/wildml"
-HOME_PAGE = "www.wildml.com/"
+PROJECT_NAME = 'projects/youtube'
+HOME_PAGE = 'https://www.youtube.com'
 DOMAIN_NAME = get_domain_name(HOME_PAGE)
 QUEUE_FILE = PROJECT_NAME + "/queue.txt"
 CRAWLED_FILE = PROJECT_NAME + "/crawled.txt"
@@ -13,3 +13,4 @@ NUMBER_OF_THREADS = 8
 queue = Queue()
 
 spider(PROJECT_NAME, HOME_PAGE, DOMAIN_NAME)
+
