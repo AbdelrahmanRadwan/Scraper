3189905d5c163e7def1348b0a0999c1a1e7a9b28
AbdelrahmanRadwan <abdelrahman.hamdy.radwan@gmail.com>
2018-04-22 03:19:48 +0200

    added the workers and their work

--- crawler/main.py
+++ crawler/main.py
@@ -1,12 +1,13 @@
 from queue import Queue
-
+import threading
 from Spider.spider import spider
 from helper_functions.domain import *
 from helper_functions.link_finder import *
 from helper_functions.files_manager import *
 
-PROJECT_NAME = 'projects/wildml'
-HOME_PAGE = 'http://www.wildml.com/'
+
+PROJECT_NAME = 'projects/youtube'
+HOME_PAGE = 'http://www.youtube.com/'
 DOMAIN_NAME = get_domain_name(HOME_PAGE)
 QUEUE_FILE = PROJECT_NAME + "/queue.txt"
 CRAWLED_FILE = PROJECT_NAME + "/crawled.txt"
@@ -18,10 +19,22 @@ spider(PROJECT_NAME, HOME_PAGE, DOMAIN_NAME)
 
 #Get someone to crawl for you
 def create_workers():
-    pass
+    for _ in range(NUMBER_OF_THREADS):
+        worker_thread = threading.Thread(target=work)
+        worker_thread.daemon = True
+        worker_thread.start()
+
+# Do the next job in the queue
+def work():
+    while True:
+        url = queue.get()
+        spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
 
 #Get something to crawl it
 def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
     queue.join()
     crawl()
 
@@ -31,8 +44,9 @@ def crawl():
     queued_links = file_to_set(QUEUE_FILE)
     if len(queued_links) > 0:
         print(str(len(queued_links)) + " links in the queue.")
-        queue.put(queued_links)
         create_jobs()
 
 
 
+create_workers()
+crawl()
